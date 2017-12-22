541742f0b5f961d071ba4d0eab4b4f3d8d901b30
jeikerxiao <jeiker@126.com>
2017-12-22 15:55:11 +0800

    add Compiler plugins
    add Compiler plugins

--- rpc-demo/rpc-demo-client/src/main/java/com/jeiker/rpc/demo/client/HelloClient.java
+++ rpc-demo/rpc-demo-client/src/main/java/com/jeiker/rpc/demo/client/HelloClient.java
@@ -2,6 +2,8 @@ package com.jeiker.rpc.demo.client;
 
 import com.jeiker.rpc.client.RpcProxy;
 import com.jeiker.rpc.demo.api.HelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,17 +14,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class HelloClient {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloClient.class);
+
     public static void main(String[] args) throws Exception {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         RpcProxy rpcProxy = context.getBean(RpcProxy.class);
 
         HelloService helloService = rpcProxy.create(HelloService.class);
         String result = helloService.hello("World");
-        System.out.println(result);
+        logger.info(result);
 
         HelloService helloService2 = rpcProxy.create(HelloService.class, "sample.hello2");
         String result2 = helloService2.hello("世界");
-        System.out.println(result2);
+        logger.info(result2);
 
         System.exit(0);
     }
